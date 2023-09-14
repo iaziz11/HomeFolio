72163500f07d26cc857b211e958f5959502d32e6
Imran Aziz <imrannoah11@gmail.com>
2023-09-13 17:14:47 -0700

    Fixed register issue

--- controllers/users.js
+++ controllers/users.js
@@ -3,8 +3,9 @@ const User = require("../models/user");
 module.exports.registerUser = async (req, res, next) => {
   const { fullName, username, password } = req.body;
   const newUser = new User({ fullName, username });
+  let registeredUser = undefined;
   try {
-    const registeredUser = await User.register(newUser, password);
+    registeredUser = await User.register(newUser, password);
   } catch (e) {
     req.flash("error", "Email is already taken, please try another or log in");
     return res.redirect("/register");
