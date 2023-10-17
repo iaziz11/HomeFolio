102198308fc1a0cbe1a084d5943f5d76db9ff687
Imran Aziz <imrannoah11@gmail.com>
2023-10-17 16:18:32 -0700

    Stop logging

--- app.js
+++ app.js
@@ -85,7 +85,9 @@ app.use((req, res, next) => {
 });
 
 // middleware
-app.use(logger("dev"));
+if (process.env.NODE_ENV === "development") {
+  app.use(logger("dev"));
+}
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
